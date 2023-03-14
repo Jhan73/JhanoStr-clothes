a6cae3574303be503c8d8e797ebf343e1c6ca6b4
JhanAntezana <jhan.antezana6174@gmail.com>
2023-03-14 11:33:10 -0500

    componente example

---
+++ src/app/components/example/example.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input ,Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.scss']
+})
+export class ExampleComponent {
+  @Input() 
+  img: string = "";
+  imgError(){
+    this.img = "https://www.w3schools.com/howto/img_avatar2.png"
+  }
+
+  @Output() 
+  carga = new EventEmitter <string>();
+  imgLoaded(){
+    console.log("el hijo sabe que cargo la imagen")
+    this.carga.emit(this.img)//emitiendo al padre
+  }
+}
